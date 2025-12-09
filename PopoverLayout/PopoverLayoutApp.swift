18bc71c4d7df55cbf7e1f220bf14cf0650c17807
John Siracusa <siracusa@gmail.com>
2025-12-09 10:43:13 -0500

    Initial Commit

---
+++ PopoverLayout/PopoverLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverLayoutApp.swift
+//  PopoverLayout
+//
+//  Created by John Siracusa on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PopoverLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
